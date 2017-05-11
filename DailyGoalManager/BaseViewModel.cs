f69ab1da11a763c454f3dbf43374348bbf234b79
jay <jayrdee@gmail.com>
2017-05-11 15:58:57 +0200

    implemented InotifyPropertyChanged and Binding, DataContext, Working OK

---
+++ DailyGoalManager/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyGoalManager
+{
+    public class BaseViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+
+
+}
--- DailyGoalManager/MainWindow.xaml.cs
+++ DailyGoalManager/MainWindow.xaml.cs
@@ -20,9 +20,20 @@ namespace DailyGoalManager
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Task t = new Task { Time = 4, TaskTitle = "Wash Dishes", Id = 2 };
+            List<Task> TaskList = new List<Task>
+            {
+            new Task { Time = 4, TaskTitle = "Wash Dishes", Id = 2 },
+            new Task { Time = 5, TaskTitle = "Laundry", Id = 4 },
+            new Task { Time = 7, TaskTitle = "Homework", Id = 6 },
+            };
+            DataContext = TaskList[2];
         }
     }
 }
